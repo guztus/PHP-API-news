093ddc87658169fa5c80da7646bae5720f61bc13
guztus <gustavs.zubars@gmail.com>
2022-11-29 18:46:49 +0200

    cleanup

--- index.php
+++ index.php
@@ -4,9 +4,7 @@ use App\Router;
 use App\TwigLoader;
 
 require_once 'vendor/autoload.php';
-//echo "<pre>";
-//var_dump(new \App\Models\Article('title', null, null, null, null, null));
-//die;
+
 (Dotenv\Dotenv::createImmutable(__DIR__))->load();
 
 Router::route(new TwigLoader());
